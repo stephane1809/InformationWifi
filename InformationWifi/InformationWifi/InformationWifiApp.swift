60d7c59a7440b0b3acee4bb880e7398efa516636
Stephane Girão <stephanegiraolinhares@gmail.com>
2024-05-23 09:55:50 -0300

    first commit

---
+++ InformationWifi/InformationWifi/InformationWifiApp.swift
@@ -0,0 +1,17 @@
+//
+//  InformationWifiApp.swift
+//  InformationWifi
+//
+//  Created by Stephane Girão Linhares on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct InformationWifiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
